29c4e8335a861d7b3dc33be1fc21a8721de72665
codeok2 <bumseok970305@gmail.com>
2025-11-27 08:44:20 +0900

    스프링 시큐리티 적용, PasswordEncoder 적용(BCrypt) 커밋

--- src/main/java/com/example/my_community/auth/Auth.java
+++ src/main/java/com/example/my_community/auth/Auth.java
@@ -1,15 +1,46 @@
 package com.example.my_community.auth;
 
-import jakarta.servlet.http.HttpSession;
-
+import com.example.my_community.auth.security.CustomUserDetails;
+import com.example.my_community.common.exception.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Optional;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
-public interface Auth {
+@Component
+public class Auth {
+    /**
+     * 현재 로그인한 유저의 ID를 필수로 가져오기
+     * 없으면 UnauthorizedException 발생
+     */
+    public Long requireUserId() {
+        Long userId = getCurrentUserId();
+        if (userId == null) {
+            throw new UnauthorizedException("로그인이 필요합니다.");
+        }
+        return userId;
+    }
 
-    Optional<Long> currentUserId(HttpServletRequest request);
+    /**
+     * 현재 로그인한 유저의 ID를 가져온다. (없으면 null 반환)
+     */
+    public Long getCurrentUserId() {
+        // 1) SecurityContext 우선
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()
+        || authentication instanceof AnonymousAuthenticationToken) {
+            return null;
+        }
 
-    Long requireUserId(HttpServletRequest request);
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomUserDetails customUserDetails) {
+                return customUserDetails.getId();
+        }
 
-    Long getOptionalUserId(HttpServletRequest request);
+        // anonymous 인 경우
+        return null;
+    }
+
+
 }
